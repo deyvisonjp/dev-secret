46a688e39399b4de45aeed44371e778bad3a8da4
Deyvison <deyvisonjp@id.uff.br>
2020-12-11 18:55:23 -0300

    Implementando rota GET /secret

--- src/handlers/secrets.js
+++ src/handlers/secrets.js
@@ -39,10 +39,56 @@ module.exports.create = async (event, context) => {
 
 };
 
-module.exports.get = async (event) => {
+module.exports.get = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
 
+  const { id: externalId } = event.pathParameters;
+  const iconmingAdminKey = event.headers['admin-key'];
+
+  try {
+    const { participants, adminKey, drawResult } = await SecretModel.findOne({
+      externalId,
+    }).select('-_id participants adminKey drawResult').lean()
+
+    // !! -> Obriga a sentença a retornar um booleano
+    // Nesse Caso, testamos primeiro se adminKey não está vazio 
+    // e depois se é igual ao registrado no banco
+    const isAdmin = !!(iconmingAdminKey && iconmingAdminKey === adminKey)
+
+
+    const result = {
+      participants,
+      hasDrew: !! drawResult.length,
+      isAdmin
+    }
+
+    // Em caso de sucesso . . . 
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    }
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        sucess: false,
+      }),
+    }
+  }
 };
 
-module.exports.draw = async (event) => {
+module.exports.draw = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
 
+  try {
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        sucess: false,
+      }),
+    }
+  }
 };
